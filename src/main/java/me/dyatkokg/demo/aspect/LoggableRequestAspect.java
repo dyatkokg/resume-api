a3fb19a658ae9e0c4f39d16c1564457c2915d0f7
Karina Dyatko <dyatkokg@gmail.com>
2022-02-17 18:04:00 +0300

    add logging

---
+++ src/main/java/me/dyatkokg/demo/aspect/LoggableRequestAspect.java
@@ -0,0 +1,30 @@
+package me.dyatkokg.demo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggableRequestAspect {
+
+    @Around("@annotation(me.dyatkokg.demo.aspect.LoggableRequest)")
+    public Object logRequestResponse(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+//        HttpServletResponse response = (((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getResponse());
+        log.info("#### Method: {}, requestURL: {}, body: {}", request.getMethod(), request.getRequestURI(), proceedingJoinPoint.getArgs());
+        Object value = proceedingJoinPoint.proceed();
+        log.info("Request result: {}", value);
+        return value;
+    }
+
+
+}
--- src/main/java/me/dyatkokg/demo/controller/ResumeController.java
+++ src/main/java/me/dyatkokg/demo/controller/ResumeController.java
@@ -1,6 +1,7 @@
 package me.dyatkokg.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.dyatkokg.demo.aspect.LoggableRequest;
 import me.dyatkokg.demo.dto.ResumeDTO;
 import me.dyatkokg.demo.service.ResumeService;
 import org.springframework.data.domain.Page;
@@ -14,33 +15,35 @@ public class ResumeController {
 
     private final ResumeService resumeService;
 
+    @LoggableRequest
     @PutMapping("create")
     public ResponseEntity<ResumeDTO> create(@RequestBody ResumeDTO resume) {
         return resumeService.create(resume);
     }
 
-
+    @LoggableRequest
     @GetMapping("find-all")
     public Page<ResumeDTO> findAll(@RequestParam(value = "size", required = false, defaultValue = "10") int size,
                                    @RequestParam(value = "page", required = false, defaultValue = "0") int page) {
         return resumeService.findAll(page, size);
     }
 
+    @LoggableRequest
     @GetMapping("{id}")
     public ResponseEntity<ResumeDTO> getById(@PathVariable("id") String id) {
         return resumeService.getById(id);
     }
 
+    @LoggableRequest
     @DeleteMapping("{id}")
     public ResponseEntity<ResumeDTO> deleteById(@PathVariable("id") String id) {
         return resumeService.deleteById(id);
     }
 
+    @LoggableRequest
     @PostMapping("update")
     public ResponseEntity<?> update(@RequestBody ResumeDTO resume) {
         return resumeService.update(resume);
     }
-
-    //логирование
-    //
+    
 }
